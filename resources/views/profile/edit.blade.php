969b2101c7e522d1c2fd9a0cb48895e6b744760d
jihadul4kbar <jihadul4kbar>
2024-06-04 16:49:31 +0800

    add passport auth

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,14 +1,14 @@
-
+@extends('layouts.app')
 
 <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Blank Page</h1>
+      <h1> {{ __('Profile') }}</h1>
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.html">Home</a></li>
           <li class="breadcrumb-item">Pages</li>
-          <li class="breadcrumb-item active">Blank</li>
+          <li class="breadcrumb-item active"> {{ __('Profile') }}</li>
         </ol>
       </nav>
     </div><!-- End Page Title -->
@@ -19,47 +19,10 @@
 
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Example Card</h5>
-              <x-app-layout>
-                <x-slot name="header">
-                    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                        {{ __('Profile') }}
-                    </h2>
-                </x-slot>
-            
-                <div class="py-12">
-                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                            <div class="max-w-xl">
-                                @include('profile.partials.update-profile-information-form')
-                            </div>
-                        </div>
-            
-                        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                            <div class="max-w-xl">
-                                @include('profile.partials.update-password-form')
-                            </div>
-                        </div>
-            
-                        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                            <div class="max-w-xl">
-                                @include('profile.partials.delete-user-form')
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </x-app-layout>
-            </div>
-          </div>
-
-        </div>
-
-        <div class="col-lg-6">
-
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Example Card</h5>
-              <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
+              <h5 class="card-title"> {{ __('Profile') }}</h5>
+              @include('profile.partials.update-profile-information-form')
+              @include('profile.partials.update-password-form')
+              @include('profile.partials.delete-user-form') 
             </div>
           </div>
 
